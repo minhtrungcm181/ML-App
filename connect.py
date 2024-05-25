2f8c60115e123fb4d02b72b73b267b0eaa94ba0f
Your Name <you@example.com>
2024-05-25 22:22:40 +0700

    timing

--- connect.py
+++ connect.py
@@ -1,18 +1,22 @@
 import json
 import string
 from random import random
+import time
 import numpy as np
 import pickle
 import re
 import paho.mqtt.client as mqtt
 from alert import *
 import requests
-broker = '172.28.182.49'
+broker = '172.28.182.55'
 MQTT_port = 1883
-url1 = "http://172.28.182.49:5000/power/alert-from-ml"
+url1 = "http://172.28.182.55:5000/power/alert-from-ml"
 topic = "topic5"
 client_id = 'Admin1'
 keepalive = 60
+message_receiver = ''
+last_alert = None
+last_device = None
 
 #generate client ID
 
@@ -24,6 +28,8 @@ model = pickle.load(open("modelA.pkl", "rb"))
 
 
 def predict(data_input):
+    global last_alert
+    global last_device
     quoted_string = re.search(r'\"([^\"]+)\"', data_input).group(1)
     temp = [float(value) for value in quoted_string.split(',')] # data format: "232, 32, 232, 23"
     data = temp[:4]
@@ -32,30 +38,36 @@ def predict(data_input):
     print(f"Working Device: {prediction}")
     if prediction != 'NO LOAD':
         alert = detect_anomalies(data, mean_values, std_values)
-        data_pub1 = alert
-        data = {
-            'alert': alert,
-            'device': prediction
-        }
-        response = requests.post(url1, data=data)
-        if response.status_code == 201:
-            print('Request successful!')
-            print('Response:', response.text)
-        else:
-            print('Request failed with status code:', response.status_code)
-        print(alert)
+        if (alert != last_alert or prediction != last_device):
+            last_alert = alert
+            last_device = prediction
+            data = {
+                'alert': alert,
+                'device': prediction
+            }
+            response = requests.post(url1, data=data)
+            if response.status_code == 201:
+                print('Request successful!')
+                print('Response:', response.text)
+            else:
+                print('Request failed with status code:', response.status_code)
+            print(alert)
+        
     else:
-        data = {
-            'alert': 'no alert',
-            'device': prediction
-        }
-        response = requests.post(url1, data=data)
-        if response.status_code == 201:
-            print('Request successful!')
-            print('Response:', response.text)
-        else:
-            print('Request failed with status code:', response.status_code)
-
+        if ('no alert' != last_alert or prediction != last_device):
+            last_alert = 'no alert'
+            last_device = prediction
+            data = {
+                'alert': 'no alert',
+                'device': prediction
+            }
+            response = requests.post(url1, data=data)
+            if response.status_code == 201:
+                print('Request successful!')
+                print('Response:', response.text)
+            else:
+                print('Request failed with status code:', response.status_code)
+        
     return
 
 
@@ -71,21 +83,18 @@ def connect_mqtt():
     client.connect(broker, MQTT_port, keepalive)
     return client
 def on_message(client, userdata, msg):
-        msg_payload = msg.payload.decode()
-        print(f"{msg.topic} {msg_payload}")
-        data_pub = str(predict(msg_payload))
+        message_receiver = msg.payload.decode()
+        predict(message_receiver)
+        # data_pub = str(predict(msg_payload))
         # publish(client,data_pub)
         print(msg)
-# def publish(client: mqtt, data_pub):
-#     client.publish("result", data_pub)
+
 def subscribe(client: mqtt):
     print(" ")
 def main():
     client = connect_mqtt()
     client.subscribe("topic5")
     client.on_message = on_message
-    # client.publish("result", data_pub_devices)
-    # client.publish("result", data_pub_alert)
     client.loop_forever()
 
 if __name__ == '__main__':
